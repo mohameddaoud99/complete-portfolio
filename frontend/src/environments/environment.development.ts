97b52287bd6d711612cd1db5607ef71ec7ebf8ac
Mohamed Tijani Daoud <mohamed.tijani.daoud@gmail.com>
2026-07-12 15:32:54 +0100

    third fix env.ts

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  mediaBaseUrl: 'http://localhost:8080'
+  //apiUrl: 'http://localhost:8080/api',
+  //mediaBaseUrl: 'http://localhost:8080'
+
+  apiUrl: 'https://portfolio-backend-q60t.onrender.com/api',
+  mediaBaseUrl: 'https://portfolio-backend-q60t.onrender.com',
 };
