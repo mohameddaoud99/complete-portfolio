5b152dc169c706af3f268a86f01c522b03465d8c
Mohamed Tijani Daoud <mohamed.tijani.daoud@gmail.com>
2026-07-12 16:06:39 +0100

    fix cors backend and frontend

--- public-site/src/environments/environment.development.ts
+++ public-site/src/environments/environment.development.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  mediaBaseUrl: 'http://localhost:8080',
-  siteUrl: 'http://localhost:4300'
+  //apiUrl: 'http://localhost:8080/api',
+  //mediaBaseUrl: 'http://localhost:8080',
+  //siteUrl: 'http://localhost:4300'
+
+  apiUrl: "https://portfolio-backend-q60t.onrender.com/api",
+  mediaBaseUrl: "https://portfolio-backend-q60t.onrender.com",
 };
