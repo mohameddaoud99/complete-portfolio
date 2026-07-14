010966831252751085eacae8d3e10c884d72e00d
Mohamed Tijani Daoud <mohamed.tijani.daoud@gmail.com>
2026-07-14 01:44:12 +0100

    fix supabase storage

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
-  mediaBaseUrl: 'http://localhost:8080',
+  //apiUrl: 'http://localhost:8080/api',
+  //mediaBaseUrl: 'http://localhost:8080',
 
-  //apiUrl: 'https://portfolio-backend-q60t.onrender.com/api',
-  //mediaBaseUrl: 'https://portfolio-backend-q60t.onrender.com',
+  apiUrl: 'https://portfolio-backend-q60t.onrender.com/api',
+  mediaBaseUrl: 'https://portfolio-backend-q60t.onrender.com',
 };
